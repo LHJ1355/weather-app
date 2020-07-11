f9bac40084266d59111efc41e9c68d7a16e2ff35
LHJ1355 <lhj56411355@gmail.com>
2020-07-12 02:33:34 +0900

    gitignore 적용

--- App.js
+++ App.js
@@ -5,8 +5,19 @@ import Loading from "./Loading";
 import Weather from "./Weather";
 import Convert from "./CoordConvert";
 import * as Location from "expo-location";
+import {ServiceKey} from './Config';
 
-const API_KEY = "40a01aabcc694e1dacde3ce940ab96a0";
+const weatherList = {
+  0 : {
+    1 : 'Clear',
+    3 : 'Clouds',
+    4 : 'Clouds' //구름 많음
+  },
+  1 : 'Rain',
+  2 : 'Hail', 
+  3 : 'Snow',
+  4 : 'Pouring',
+}
 
 export default class App extends React.Component {
   state = {
@@ -16,48 +27,54 @@ export default class App extends React.Component {
     REH : null,
     T3H : null,
   }
-  getWeather = async(nx, ny) => {
-    let PTY, SKY;
-    const PtyObj = {
-      0 : {
-        1 : 'Clear',
-        3 : 'Clouds',
-        4 : 'Clouds' //구름 많음
-      },
-      1 : 'Rain',
-      2 : 'Hail', 
-      3 : 'Snow',
-      4 : 'Pouring',
-    }
-    const servicesKey = '=OCR%2BQp8x726v73%2FFhqmfrsPBzILqu4VruxM%2FUMb5%2FbS4h1KBZ%2BuKTwgZlgmL0exU75LVCmkpxOuozWg9jdnwYw%3D%3D'
+  getUrl = (nx, ny) => {
+    let today = new Date();
+
+    let year = today.getFullYear().toString();
+    let month = today.getMonth() + 1;
+    let strMonth = ((month < 10) ? '0' + month.toString() : month.toString());
+    let date = today.getDate();
+    let strDate = ((date < 10) ? '0' + date.toString() : date.toString());
+    let day = year + strMonth + strDate;
+
+    let time = today.getHours();
+    if(time >= 3 && time <6) time = "0200";
+    else if(time >= 6 && time < 9) time = "0500";
+    else if(time >= 9 && time < 12) time = "0800";
+    else if(time >= 12 && time < 15) time = "1100";
+    else if(time >= 15 && time < 18) time = "1400";
+    else if(time >= 18 && time < 21) time = '1700';
+    else if(time >= 21 && time < 24) time = "2000";
+    else time = "0200";
+    
     var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst';
-    var queryParams = '?' + encodeURIComponent('ServiceKey') + servicesKey; /* Service Key*/
+    var queryParams = '?' + encodeURIComponent('ServiceKey') + serviceKey; /* Service Key*/
     queryParams += '&' + encodeURIComponent('ServiceKey') + '=' + encodeURIComponent('-'); /* */
     queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
     queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
     queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('JSON'); /* */
-    queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent('20200706'); /* */
-    queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent('1700'); /* */
+    queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent(`${day}`); /* */
+    queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent(`${time}`); /* */
     queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent(`${nx}`); /* */
     queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent(`${ny}`); /* */
-    const {data : { response : { body : { items : {item}}}}} = await axios.get(url + queryParams);
-    PTY = item[1].fcstValue;
-    SKY = item[3].fcstValue;
+    return url + queryParams;
+  }
+  getWeather = async(nx, ny) => {
+    let PTY, SKY;
+    const url = this.getUrl(nx, ny);
+    const {data : { response : { body : { items : {item}}}}} = await axios.get(url);
+    
+    let items = item.filter((i) => (i.category != 'R06' && i.category != 'S06'))
+    PTY = items[1].fcstValue;
+    SKY = items[3].fcstValue;
+    console.log(PTY, SKY);
     this.setState({
       isLoading : false,
-      condition : (PTY ? PtyObj[PTY] : PtyObj[PTY][SKY]),
-      POP : item[0].fcstValue,
-      REH : item[2].fcstValue,
-      T3H : item[4].fcstValue,
+      condition : ((PTY != 0) ? weatherList[PTY] : weatherList[PTY][SKY]),
+      POP : parseInt(items[0].fcstValue),
+      REH : parseInt(items[2].fcstValue),
+      T3H : parseInt(items[4].fcstValue),
     })
-    /*const {data : {main : {temp}, weather}} = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
-      );
-      this.setState({
-        isLoading : false,
-        temp : temp,
-        condition : weather[0].main,
-      })*/
   }
   getLocation = async () => {
     let rs;
